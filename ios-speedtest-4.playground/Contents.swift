5babb135d825d70de988b840da5be950665b4e8b
bodour Alrashidi <bodouralrashidi@gmail.com>
2020-06-24 08:15:39 +0300

    Speedtest4 done

---
+++ ios-speedtest-4.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+// MARK: هذي الداله تقوم بضرب رقمين و ترجع الناتج
+
+func Multiplication(number1 : Int , number2 : Int)-> Int
+{
+    
+    return number1 * number2
+}
+
+
+// MARK:  و مرر رقمين ثم اطبع اناتج الضرب  Multiplication قم باستدعاء الداله
